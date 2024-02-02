3e9b6291c96e6ef2346ce22bde36a65b4d57da7f
Kaidan <kaidan13th@gmail.com>
2024-02-02 19:50:08 +0200

    emailing implemented

--- src/app/api/test/route.js
+++ src/app/api/test/route.js
@@ -1,3 +1,18 @@
-export default async function handler(req, res) {
-  return "hello";
+import { Resend } from 'resend';
+
+export async function POST(req) {
+    const resend = new Resend('re_YzKcEsW2_HmsrKjDsf2yhs6bVZchEazEX');
+
+    const data = await req.json();
+
+    console.log(data);
+
+    resend.emails.send({
+      from: 'onboarding@resend.dev',
+      to: data.email,
+      subject: data.subject,
+      html: data.content//`${data.subtitle}\n\n${data.content}`
+    });
+
+    return Response.json("hello");
 }
